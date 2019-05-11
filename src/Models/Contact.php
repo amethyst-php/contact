8c1dd13108b94e86f18496d4336ebacd99011fec
railken <railken@protonmail.ch>
2019-05-11 17:01:36 +0200

    fix style

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -11,7 +11,8 @@ use Railken\Lem\Contracts\EntityContract;
 
 class Contact extends Model implements EntityContract
 {
-    use SoftDeletes, ConfigurableModel;
+    use SoftDeletes;
+    use ConfigurableModel;
 
     /**
      * Create a new Eloquent model instance.
