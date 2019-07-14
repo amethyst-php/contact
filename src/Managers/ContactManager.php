d8d8df118306d34fc6fbc09a2f54f8000b8d25e4
railken <railken@protonmail.ch>
2019-07-14 13:07:31 +0200

    fix style

--- src/Managers/ContactManager.php
+++ src/Managers/ContactManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Contact newEntity()
- * @method \Amethyst\Schemas\ContactSchema getSchema()
+ * @method \Amethyst\Models\Contact                 newEntity()
+ * @method \Amethyst\Schemas\ContactSchema          getSchema()
  * @method \Amethyst\Repositories\ContactRepository getRepository()
- * @method \Amethyst\Serializers\ContactSerializer getSerializer()
- * @method \Amethyst\Validators\ContactValidator getValidator()
- * @method \Amethyst\Authorizers\ContactAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\ContactSerializer  getSerializer()
+ * @method \Amethyst\Validators\ContactValidator    getValidator()
+ * @method \Amethyst\Authorizers\ContactAuthorizer  getAuthorizer()
  */
 class ContactManager extends Manager
 {
