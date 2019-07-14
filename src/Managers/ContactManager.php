6c379b4e7ade4cfad54afca143bb218a61d5f627
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/ContactManager.php
+++ src/Managers/ContactManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Contact newEntity()
+ * @method \Amethyst\Schemas\ContactSchema getSchema()
+ * @method \Amethyst\Repositories\ContactRepository getRepository()
+ * @method \Amethyst\Serializers\ContactSerializer getSerializer()
+ * @method \Amethyst\Validators\ContactValidator getValidator()
+ * @method \Amethyst\Authorizers\ContactAuthorizer getAuthorizer()
+ */
 class ContactManager extends Manager
 {
     use ConfigurableManager;
